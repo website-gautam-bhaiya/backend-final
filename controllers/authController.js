0225bc4b66e88941d34f2801d567c7055f58f3bb
Prasoon Upadhyay <uprasoon@gmail.com>
2024-04-25 21:25:14 +0530

    Changed sameSite attribute in Cookie Options

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,8 @@ const createSendToken = (user, statusCode, res) => {
     {
         expires: new Date( Date.now() + process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 100,
         httpOnly: true, 
-        sameSite: 'strict'
+        // sameSite: 'strict'
+        sameSite: 'none'
     }
 
     if(process.env.NODE_ENV === 'production') cookieOptions.secure = true
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-
+const fs = require('fs')
 const app = require('./app') 
-
+const https = require('https')
 dotenv.config( { path: ".env" } )
 
+const options = {
+    key: fs.readFileSync('./../ssl--certificates/decrypted-key.pem'),
+    cert: fs.readFileSync('./../ssl--certificates/cert.pem')
+};
+
+// const server = https.createServer(options, app)
 
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
 
@@ -15,7 +21,7 @@ mongoose.connect(DB, {
 }).then( conn => console.log("DB Connection Successful!"))
 
 const port = process.env.PORT || 3000;  
-const server = app.listen(port,  () => console.log(`App running on PORT:${port}...`))
+const server = https.createServer(options, app).listen(port,  () => console.log(`App running on PORT:${port}...`))
 
 process.on('unhandledRejection', err => {
     
