239387c227885bb18fab0a6ee9f6a944747c2aa9
Prasoon Upadhyay <uprasoon@gmail.com>
2024-06-07 08:32:41 +0530

    Server Configured for Render

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = require('./app')
 const https = require('https')
 dotenv.config( { path: ".env" } )
 
-const options = {
-    key: fs.readFileSync('./../ssl--certificates/decrypted-key.pem'),
-    cert: fs.readFileSync('./../ssl--certificates/cert.pem')
-};
+// const options = {
+//     key: fs.readFileSync('./../ssl--certificates/decrypted-key.pem'),
+//     cert: fs.readFileSync('./../ssl--certificates/cert.pem')
+// };
 
 
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
@@ -20,9 +20,9 @@ mongoose.connect(DB, {
 }).then( conn => console.log("DB Connection Successful!"))
 
 const port = process.env.PORT || 3000;  
-const server = https.createServer(options, app).listen(port,  () => console.log(`App running on PORT:${port}...`))
+// const server = https.createServer(options, app).listen(port,  () => console.log(`App running on PORT:${port}...`))
 
-// const server = app.listen(port, () => console.log(`App running on PORT:${port}...`))
+const server = app.listen(port, () => console.log(`App running on PORT:${port}...`))
 
 process.on('unhandledRejection', err => {
     
