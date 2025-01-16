3b1cf37d9b8e85f812774300b8de39b8a373da4b
website-gautam-bhaiya <workthinkfinance@gmail.com>
2025-01-16 14:15:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(DB, {
 const port = process.env.PORT || 3000;  
 // const server = https.createServer(options, app).listen(port,  () => console.log(`App running on PORT:${port}...`))
 
-const server = app.listen(port, () => console.log(`App running on PORT:${port}...`))
+// const server = app.listen(port, () => console.log(`App running on PORT:${port}...`))
 
 process.on('unhandledRejection', err => {
     
@@ -31,3 +31,5 @@ process.on('unhandledRejection', err => {
         process.exit(1)
     })    
 })
+
+module.exports = app;
