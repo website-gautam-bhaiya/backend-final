166f1b5903d5efa03aaf2b4b1c39506f73188db8
website-gautam-bhaiya <workthinkfinance@gmail.com>
2025-01-16 14:45:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const https = require('https');
 dotenv.config({ path: ".env" });
 
 // SSL options (uncomment if using HTTPS)
-const options = {
-    key: fs.readFileSync('./../ssl--certificates/decrypted-key.pem'),
-    cert: fs.readFileSync('./../ssl--certificates/cert.pem')
-};
+//const options = {
+//    key: fs.readFileSync('./../ssl--certificates/decrypted-key.pem'),
+//    cert: fs.readFileSync('./../ssl--certificates/cert.pem')
+//};
 
 // MongoDB connection
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
@@ -19,9 +19,9 @@ mongoose.connect(DB, {
 
 // Server setup (choose either HTTP or HTTPS)
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`App running on PORT:${port}...`));
+//const server = app.listen(port, () => console.log(`App running on PORT:${port}...`));
 // If using HTTPS, uncomment below and comment the HTTP line
-// const server = https.createServer(options, app).listen(port, () => console.log(`App running on PORT:${port}...`));
+const server = https.createServer(options, app).listen(port, () => console.log(`App running on PORT:${port}...`));
 
 process.on('unhandledRejection', err => {
     console.log(err.name, err.message);
